6ac9adce88061ac0c0421173a7970b387ee70c6f
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2023-11-01 02:29:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [value, setValue] = useState();
+
+  return (
+    <div className="App">
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <div
+        className="circle"
+        style={{
+          backgroundImage: `conic-gradient(red ${value}%, transparent 0)`
+        }}
+      ></div>
+    </div>
+  );
+}
